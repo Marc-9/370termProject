93e5c424b282f71222c327511262af87a3b40bef
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2020-11-02 16:29:19 -0700

    Update index.php

--- www/html/index.php
+++ www/html/index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "Hello World";
+$requestType = $_SERVER['REQUEST_METHOD'];
+if($requestType == "POST"){
+	$postBody = file_get_contents("php://input");
+	echo $postBody."\n";
+	http_response_code(200);
+}
+else if($requestType == "GET"){
+	echo "Hello World\n";
+	http_response_code(200);
+}
+else{
+	http_response_code(405);
+}
 
 ?>
